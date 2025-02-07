d158d6c24b070973ec70961dc2d24b2fcb36138d
Erica Rodrigues <ericamrodrigs@gmail.com>
2025-02-07 19:50:40 -0300

    :tada: Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Erica Rodrigues on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Erica Rodrigues on 07/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
