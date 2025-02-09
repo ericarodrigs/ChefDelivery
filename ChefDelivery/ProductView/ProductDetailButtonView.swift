51fb0d37b773b14f78f256adcda17f7cf6c32380
Erica Rodrigues <ericamrodrigs@gmail.com>
2025-02-09 18:02:10 -0300

    :recycle: extract some components

---
+++ ChefDelivery/ProductView/ProductDetailButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  ProductDetailButtonView.swift
+//  ChefDelivery
+//
+//  Created by Erica Rodrigues on 09/02/25.
+//
+
+import SwiftUI
+
+struct ProductDetailButtonView: View {
+    var body: some View {
+        Button {
+            print("O botão foi pressionado")
+        } label: {
+            HStack(spacing: 8) {
+                Image(systemName: "cart")
+                
+                Text("Adicionar ao carrinho")
+            }
+            .padding(.horizontal, 32)
+            .padding(.vertical, 16)
+            .font(.title3.bold())
+            .background(Color("ColorRed"))
+            .foregroundColor(.white)
+            .cornerRadius(32)
+            .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10, x: 6, y: 8)
+        }
+    }
+}
+
+struct ProductDetailButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailButtonView()
+    }
+}
--- ChefDelivery/ProductView/ProductDetailView.swift
+++ ChefDelivery/ProductView/ProductDetailView.swift
@@ -10,80 +10,18 @@ import SwiftUI
 struct ProductDetailView: View {
     
     let product: ProductType
-    @State private var productQuantity = 1
     
     var body: some View {
         VStack {
-            VStack(alignment: .leading, spacing: 16) {
-                Image(product.image)
-                    .resizable()
-                    .scaledToFit()
-                    .shadow(radius: 20)
-                
-                Text(product.name)
-                    .font(.title)
-                    .bold()
-                    .padding(.horizontal)
-                    .padding(.top)
-                
-                Text(product.description)
-                    .padding(.horizontal)
-                
-                Text(product.formattedPrice)
-                    .font(.title3)
-                    .bold()
-                    .padding(.horizontal)
-            }
+            ProductDetailHeaderView(product: product)
             
             Spacer()
             
-            VStack(spacing: 16) {
-                Text("Quantidade")
-                    .font(.title2)
-                    .bold()
-                
-                HStack {
-                    Button {
-                        if productQuantity > 1 {
-                            productQuantity -= 1
-                        }
-                    } label: {
-                        Image(systemName: "minus.circle.fill")
-                            .font(.title.bold())
-                    }
-                    
-                    Text("\(productQuantity)")
-                        .font(.title2)
-                        .bold()
-                    
-                    Button {
-                        productQuantity += 1
-                    } label: {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.title.bold())
-                    }
-
-                }
-            }
+            ProductDetailQuantityView()
             
             Spacer()
             
-            Button {
-                print("O botão foi pressionado")
-            } label: {
-                HStack(spacing: 8) {
-                    Image(systemName: "cart")
-                    
-                    Text("Adicionar ao carrinho")
-                }
-                .padding(.horizontal, 32)
-                .padding(.vertical, 16)
-                .font(.title3.bold())
-                .background(Color("ColorRed"))
-                .foregroundColor(.white)
-                .cornerRadius(32)
-                .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10, x: 6, y: 8)
-            }
+            ProductDetailButtonView()
         }
         .padding(.bottom, 16)
     }
