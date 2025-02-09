7e8db88ea3da34b6794ab74733119571e0f3a38b
Erica Rodrigues <ericamrodrigs@gmail.com>
2025-02-09 11:37:58 -0300

    :construction: add NavigationView to navigate do StoreDetailView

---
+++ ChefDelivery/StoresView/StoreDetailView.swift
@@ -0,0 +1,23 @@
+//
+//  StoreDetailView.swift
+//  ChefDelivery
+//
+//  Created by Erica Rodrigues on 09/02/25.
+//
+
+import SwiftUI
+
+struct StoreDetailView: View {
+    
+    let store: OrderType
+    
+    var body: some View {
+        Text(store.name)
+    }
+}
+
+struct StoreDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreDetailView(store: storesMock[0])
+    }
+}
