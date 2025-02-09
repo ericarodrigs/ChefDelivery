0b5035b6c35cecc36aad83ebb7848cca36a5ebd6
Erica Rodrigues <ericamrodrigs@gmail.com>
2025-02-09 11:51:52 -0300

    :construction: add news structs and refactor code

---
+++ ChefDelivery/Model/ProductType.swift
@@ -0,0 +1,16 @@
+//
+//  ProductType.swift
+//  ChefDelivery
+//
+//  Created by Erica Rodrigues on 09/02/25.
+//
+
+import Foundation
+
+struct ProductType: Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let image: String
+    let price: Double    
+}
