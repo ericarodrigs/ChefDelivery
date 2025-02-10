518edc49981a6c6847a3011dfdebb5a31ea76819
Erica Rodrigues <ericamrodrigs@gmail.com>
2025-02-10 19:54:50 -0300

    :sparkles: finish animations in HomeView

--- ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/HomeView/HomeView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var isAnimating = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var buttonOffset: CGFloat = 0
+    @State private var showSecondScreen = false
+    let buttonHeight: CGFloat = 80
     
     var body: some View {
         GeometryReader { geometry in
@@ -34,7 +38,7 @@ struct HomeView: View {
                 
                 VStack {
                     Text("Chef Delivery")
-                        .font(.system(size: 40)).fontWeight(.heavy)
+                        .font(.system(size: 48)).fontWeight(.heavy)
                         .foregroundColor(Color("ColorRed"))
                         .opacity(isAnimating ? 1 : 0)
                         .offset(y: isAnimating ? 0 : -40)
@@ -46,8 +50,94 @@ struct HomeView: View {
                         .foregroundColor(.black.opacity(0.7))
                         .opacity(isAnimating ? 1 : 0)
                         .offset(y: isAnimating ? 0 : -40)
+
+                    Image("image")
+                        .resizable()
+                        .scaledToFit()
+                        .shadow( radius: 60)
+                        .padding(isAnimating ? 32 : 92)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(x: imageOffset.width, y: imageOffset.height)
+                        .gesture(
+                            DragGesture ()
+                                .onChanged({ gesture in
+                                    withAnimation(.easeInOut(duration: 0.5)) {
+                                        imageOffset = gesture.translation
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    withAnimation(.easeInOut(duration: 0.5)) {
+                                        imageOffset = .zero
+                                    }
+                                })
+                        )
                     
-                    Spacer()
+                    ZStack {
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                        
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Descubra mais")
+                            .font(.title2)
+                            .bold()
+                            .offset(x: 20)
+                            .foregroundColor(Color("ColorRedDark"))
+                        
+                        HStack {
+                            Capsule()
+                                .fill(Color("ColorRed"))
+                                .frame(width: buttonOffset + buttonHeight)
+                            
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(Color("ColorRed"))
+                                          
+                                Circle()
+                                    .fill(Color("ColorRedDark"))
+                                    .padding(8)
+                                
+                                Image(systemName: "chevron.right.2")
+                                    .font(.system(size: 24).bold())
+                                    .foregroundColor(.white)
+                            }
+                            .frame(width: buttonHeight)
+                            
+                            Spacer()
+                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    
+                                    if gesture.translation.width >= 0 && buttonOffset <= (geometry.size.width - 60) - buttonHeight {
+                                            withAnimation(.easeInOut(duration: 0.25)) {
+                                            buttonOffset = gesture.translation.width
+                                            }
+                                        }
+                                })
+                                .onEnded({ _ in
+                                    
+                                    if buttonOffset > (geometry.size.width - 60) / 2 {
+                                        showSecondScreen = true
+                                    } else {
+                                        withAnimation(.easeInOut(duration: 0.25)) {
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                })
+                        )
+                    }
+                    .frame(width: geometry.size.width - 60, height: buttonHeight)
+                    .padding(.bottom, 32)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(y: isAnimating ? 0 : 100)
                 }
             }
             .onAppear {
@@ -55,6 +145,9 @@ struct HomeView: View {
                     isAnimating = true
                 }
         }
+            .fullScreenCover(isPresented: $showSecondScreen) {
+                ContentView()
+            }
         }
     }
 }
