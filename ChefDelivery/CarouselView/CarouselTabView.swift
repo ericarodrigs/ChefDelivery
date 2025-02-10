7c58f39538d88a329f8e0259bee49269e035a34f
Erica Rodrigues <ericamrodrigs@gmail.com>
2025-02-10 20:35:59 -0300

    :sparkles: add timer and animation in CarouselTabView

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    
+    @State private var currentIndex = 1
+    
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(ordersBannerMock) {orderBanner in
                 CarouselItemView(order: orderBanner)
+                    .tag(orderBanner.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {
+                _ in
+                
+                withAnimation(.easeInOut(duration: 1)) {
+                    if currentIndex > ordersMock.count {
+                        currentIndex = 1
+                    }
+                    currentIndex += 1
+                }
+            }
+        }
     }
 }
 
